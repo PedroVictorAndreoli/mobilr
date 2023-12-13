f95ab9a2e05b54d9452d7466aa4559088bfcde2d
PedroVictorAndreoli <pvandreoli@gmail.com>
2023-12-12 21:28:24 -0300

    Pronto

--- app/src/main/java/com/example/usandosqlite_pm25s_2023/MainActivity.kt
+++ app/src/main/java/com/example/usandosqlite_pm25s_2023/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.example.usandosqlite_pm25s_2023
 
+import android.Manifest
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.sqlite.SQLiteDatabase
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import com.example.usandosqlite_pm25s_2023.database.DatabaseHandler
 import com.example.usandosqlite_pm25s_2023.entity.Locate
 import java.lang.StringBuilder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , LocationListener {
 
     private lateinit var etCod : EditText
     private lateinit var etNome : EditText
     private lateinit var etTelefone : EditText
     private lateinit var etDescricao: EditText
-
+    private lateinit var locationManager : LocationManager
     private lateinit var banco : DatabaseHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +40,39 @@ class MainActivity : AppCompatActivity() {
         etTelefone = findViewById( R.id.etTelefone )
         etDescricao = findViewById(R.id.etDescricao)
         banco = DatabaseHandler( this )
+        locationManager = getSystemService( Context.LOCATION_SERVICE ) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0f,this)
+
+        ActivityCompat.requestPermissions( this, arrayOf( Manifest.permission.ACCESS_FINE_LOCATION ), 1 )
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
 
         if ( intent.getIntExtra( "cod",0 ) != 0 ) {
             val cod = intent.getIntExtra("cod", 0)
@@ -108,5 +150,10 @@ class MainActivity : AppCompatActivity() {
         etNome.setText( "" )
         etTelefone.setText( "" )
         etDescricao.setText("")
+    }
+
+    override fun onLocationChanged(position: Location) {
+        etNome.setText( position.latitude.toString() )
+        etTelefone.setText( position.longitude.toString() )
     }
 }
